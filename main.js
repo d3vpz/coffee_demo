993e5cad74739eeb2d00359546f1b58f383de456
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-02-15 02:24:23 +0000

    update

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ engine.displayWindow();
 engine.fillColor = 'black';
 engine.fillRect(0, 0, 320, 240);
 
-let sprite = new CoffeeSprite('logo', 225, 225);
-engine.drawSprite(
-    sprite,
-    engine.window.width / 2 - sprite.width / 2,
-    engine.window.height / 2 - sprite.height / 2
-);
+let logo = new CoffeeObject();
+logo.setSprite(new CoffeeSprite('logo', 225, 225));
+logo.goTo(50, 0);
+logo.scaleTo(225, 225);
+logo.rotateTo(rad(45));
+engine.drawObject(logo);
