aa09c208e781ae0f851e60d5e51c770dc1df1175
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-02-23 16:53:37 +0000

    update lib

--- coffee_update.sh
+++ coffee_update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 cd lib
 
 wget https://raw.githubusercontent.com/d3vpz/coffee/main/lib/coffee_main.js -O coffee_main.js
 wget https://raw.githubusercontent.com/d3vpz/coffee/main/lib/coffee_math.js -O coffee_math.js
 
-BOLD=$(tput bold)
+cd ..
+
+wget https://raw.githubusercontent.com/d3vpz/coffee/main/coffee_update.sh -O coffee_update.sh
+wget https://raw.githubusercontent.com/d3vpz/coffee/main/package.sh -O package.sh
+
 GREEN='\033[1;32m'
-echo "${GREEN}Coffee has been updates successfully.\n"
+echo "${GREEN}Coffee has been updated successfully.\n"
---
+++ package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo zip -r $1.zip .
+
+GREEN='\033[1;32m'
+echo "${GREEN}Project successfully packaged.\n"
